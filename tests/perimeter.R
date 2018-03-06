ef6059af2e7eb8885c75de2782290d5b5dba9bb5
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-03-06 11:02:13 +0100

    fix perimeter test

--- tests/perimeter.R
+++ tests/perimeter.R
@@ -1,5 +1,6 @@
 suppressPackageStartupMessages(library(lwgeom))
 suppressPackageStartupMessages(library(sf))
 demo(nc, ask = FALSE)
+nc = st_transform(nc, 3857)
 st_perimeter(nc)[1:5]
 st_perimeter_2d(nc)[1:5]
