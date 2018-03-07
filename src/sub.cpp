b9191788d9f4a4a9a984a4b6cc8051c6941b8a90
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-03-07 10:14:34 +0100

    add sub.cpp

---
+++ src/sub.cpp
@@ -0,0 +1,31 @@
+#include <Rcpp.h>
+
+#include <string.h>
+
+extern "C" {
+#include <liblwgeom.h>
+}
+
+using namespace Rcpp;
+
+#include "lwgeom.h" // sfc <--> lwgeom
+
+// [[Rcpp::export]]
+Rcpp::List CPL_linesubstring(Rcpp::List sfc, double from, double to, double tolerance = 0.0) {
+	std::vector<LWGEOM *> lw = lwgeom_from_sfc(sfc);
+	std::vector<LWGEOM *> out(sfc.size());
+	for (size_t i = 0; i < lw.size(); i++) {
+		LWLINE *iline = lwgeom_as_lwline(lw[i]);
+		if ( lw[i]->type == LINETYPE ) {
+			POINTARRAY *opa;
+			opa = ptarray_substring(((LWLINE*)lw[i])->points, from, to, tolerance);
+        	if ( opa->npoints == 1 ) /* Point returned */
+            	out[i] = (LWGEOM *)lwpoint_construct(lw[i]->srid, NULL, opa);
+        	else
+            	out[i] = (LWGEOM *)lwline_construct(lw[i]->srid, NULL, opa);
+		} else
+			stop("geometry should be of LINE type");
+		lwgeom_free(lw[i]);
+	}
+	return sfc_from_lwgeom(out);
+}
