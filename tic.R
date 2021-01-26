d314b86bf1fdb4ee832a7b438f8fd71cb6c06334
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2021-01-26 18:12:33 +0100

    add tic

--- tic.R
+++ tic.R
@@ -1,6 +1,8 @@
-add_package_checks()
+# installs dependencies, runs R CMD check, runs covr::codecov()
+do_package_checks()
 
-###
-# deploy pkgdowm site
-###
-do_pkgdown(document = FALSE)
+if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+  # creates pkgdown site and pushes to gh-pages branch
+  # only for the runner with the "BUILD_PKGDOWN" env var set
+  do_pkgdown()
+}
